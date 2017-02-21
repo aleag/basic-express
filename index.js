90d58230a7ed2c8cff295bc941302ce843f9509b
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-02-21 18:18:09 +0000

    Fixing comments

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-# View engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-# Middleware 1
+// Middleware 1
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
   var browser_ = req.headers['user-agent'];
   var hostName = req.hostname;
 
-  res.render('hello', { ip: ip_, browser: browser_, host-name: hostName});
+  res.render('hello', { ip: ip_, browser: browser_, hostname: hostName});
 });
 
 var server = app.listen(8080, function () {
